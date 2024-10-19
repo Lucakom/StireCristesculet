7a6921405a06d1799cdc9ea4fb34d8dfe9e9812c
Lucakom <cotunaluca76@gmail.com>
2024-10-19 12:58:54 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("reveal").addEventListener("click", function() {
+    const story = document.getElementById("story");
+    story.innerHTML += `<p>Cristescu era cunoscut în cartier ca fiind un om cu suflet mare, mereu gata să ajute pe cei din jur. Însă, ca orice om, și el avea momente de inspirație, unele mai neobișnuite decât altele. Într-o dimineață însorită, pe când stătea pe verandă, a observat pomul robust ce se înălța chiar în fața casei sale. Deodată, o idee ciudată a apărut în mintea sa, o fantezie absurdă, dar care părea să-i aducă o oarecare distracție.
+După ce s-a consultat cu sine, Cristescu s-a gândit că ar fi amuzant să-și îndeplinească dorința de a experimenta o conexiune stranie cu natura. Astfel, el a acționat impulsiv, fără să se gândească la consecințe, și a decis să se apropie de pom și să-și atingă anusul de trunchiul lui. O acțiune pătimașă, dar care, în mintea lui, promitea o reverie inedită.
+Dar, așa cum se întâmplă adesea cu ideile impulsive, această ispravă nu a mers conform planului. Cristescu nu s-a gândit la rigorile unei asemenea acțiuni și surpriza a fost că, în urma acestui experiment bizar, simțea o presiune mai mare decât se aștepta. Așa că s-a trezit, câteva minute mai târziu, pe drum spre spital, realizând că acțiunea sa impulsivă i-a lărgit mai mult decât capacitățile fizice.
+Ajuns la spital, între doctori și asistentă s-a creat o atmosfera de confuzie și râsete nervoase. Cristescu, cu un zâmbet stânjenit pe față, a început să povestească cum un simplu pom a fost motivul unei aventuri nebunești, care ar fi rămas la stadiul de fantezie, dar care s-a transformat într-o experiență neplăcută și hilară deopotrivă.
+Însă, dincolo de toate cele întâmplate, Cristescu a învățat o lecție importantă despre curiozitate și responsabilitate. De la acea zi, el nu a mai privit pomul din fața casei sale la fel. Făcând haz de necaz, s-a hotărât să își păstreze ideile ciudate pentru videoclipurile sale virale, în loc să le pună în practică. Și, deși a ieșit din spital cu o poveste pe care nu și-ar fi dorit-o, a realizat că uneori, viața îți oferă cele mai neobișnuite ocazii de a râde, chiar și din cele mai bizare situații.
+
+Și așa Cristescu a devenit un personaj legendar în cartier, iar pomul din fața casei sale a rămas un simbol al aventurilor sale neobișnuite.</p>`;
+    this.style.display = 'none'; 
+}); 
